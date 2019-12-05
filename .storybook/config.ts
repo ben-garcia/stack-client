cfdb7feace7c6578647e2c006d992d3daef4fde1
= <7rubengarcia7@gmail.com>
2019-12-05 09:08:38 -0800

    Added Enzyme to test stories

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,4 +1,4 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.tsx
+// automatically import all stories.tsx files.
 configure(require.context('../src/components', true, /stories.tsx$/), module);
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  testMatch: ['/components/**/*test.(ts|tsx)', '**/?(*.)+(test).+(ts|tsx)'],
+  testMatch: ['/components/**/*test.tsx', '**/?(*.)+(test).+(ts|tsx)'],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
   },
+  setupFilesAfterEnv: ['./setupTests.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules',
+    '<rootDir>/.storybook',
+    '<rootDir>/setupTests.ts',
+  ],
   moduleNameMapper: { '\\.(scss)$': '<rootDir>/src/__mocks__/styles.ts' },
 };
