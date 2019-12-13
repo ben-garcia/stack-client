f57280d30172898f2257d30a962d4337354a854f
= <7rubengarcia7@gmail.com>
2019-12-13 10:17:52 -0800

    [Added](Form): story 'with FormGroup'

--- src/components/Form/stories.tsx
+++ src/components/Form/stories.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Form from '.';
+import FormGroup from './FormGroup';
 
-storiesOf('Form', module).add('default', () => (
-  <Form>
-    <label htmlFor="name">Name:</label>
-    <input type="text" id="name" />
-  </Form>
-));
+storiesOf('Form', module)
+  .add('default', () => (
+    <Form>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" />
+    </Form>
+  ))
+  .add('with FormGroup', () => (
+    <FormGroup>
+      <input />
+      <input />
+    </FormGroup>
+  ));
