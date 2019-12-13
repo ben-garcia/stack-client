873baada0c9fbabc13dcedb770a81028680f04ff
= <7rubengarcia7@gmail.com>
2019-12-13 11:50:39 -0800

    [Added](FormGroup): comment about the component

--- src/components/Form/FormGroup/index.tsx
+++ src/components/Form/FormGroup/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { FormGroupProps } from './types';
 import './styles.scss';
 
+/**
+ * FromGroup displays a collection of related input fields.
+ */
 const FormGroup: React.FC<FormGroupProps> = ({ children }) => {
   return <fieldset className="form-group">{children}</fieldset>;
 };
