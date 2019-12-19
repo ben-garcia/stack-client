f7ed88cc5dbfa59b18eae78853a74975440f166f
= <7rubengarcia7@gmail.com>
2019-12-19 14:25:57 -0800

    [Update](FormGroup): added flexDirection prop

--- src/components/Form/FormGroup/index.tsx
+++ src/components/Form/FormGroup/index.tsx
@@ -6,11 +6,19 @@ import './styles.scss';
 /**
  * FromGroup displays a collection of related input fields.
  */
-const FormGroup: React.FC<FormGroupProps> = ({ children, className = '' }) => {
+const FormGroup: React.FC<FormGroupProps> = ({
+  children,
+  className = '',
+  flexDirection = 'row',
+}) => {
   let classNamesToAdd: string = 'form__group';
 
   if (className.trim() !== '') {
     classNamesToAdd += ` ${className}`;
+  }
+
+  if (flexDirection === 'column') {
+    classNamesToAdd += ' form__group--column';
   }
 
   return <fieldset className={classNamesToAdd}>{children}</fieldset>;
--- src/components/Form/FormGroup/types.ts
+++ src/components/Form/FormGroup/types.ts
@@ -1,4 +1,5 @@
 export interface FormGroupProps {
   children: React.ReactNode[] | React.ReactNode;
   className?: string;
+  flexDirection?: 'row' | 'column';
 }
