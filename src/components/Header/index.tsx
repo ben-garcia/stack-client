fafdcd4c5f99af61d988e2743db176f3fbe011be
= <7rubengarcia7@gmail.com>
2019-12-19 15:08:37 -0800

    [Update](Header): added 'textAlign' prop to control the heading element

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,47 +5,73 @@ import { HeaderProps } from './types';
 
 const Header: React.FC<HeaderProps> = ({
   className = '',
-  headingClassName = '',
   heading,
   as = 'h1',
+  textAlign = 'left',
   children,
 }) => {
+  // classnames that are to be added to the heading itself.
+  let classNamesToAdd: string = 'header';
+
+  if (className.trim() !== '') {
+    classNamesToAdd += ` ${className}`;
+  }
+
+  // classnames to the heading element which is dependent on textAlign prop.
+  let headingClassNamesToAdd: string = 'header__heading--left';
+
+  if (textAlign === 'center') {
+    headingClassNamesToAdd = 'header__heading--center';
+  } else if (textAlign === 'right') {
+    headingClassNamesToAdd = 'header__heading--right';
+  }
+
   // variable will contain the correct heading tag.
   let headingToRender: JSX.Element = (
-    <h1 className={`header__heading ${headingClassName}`}>{heading}</h1>
+    <h1 className={`header__heading ${headingClassNamesToAdd}`}>{heading}</h1>
   );
 
   switch (as) {
     case 'h2':
       headingToRender = (
-        <h2 className={`header__heading ${headingClassName}`}>{heading}</h2>
+        <h2 className={`header__heading ${headingClassNamesToAdd}`}>
+          {heading}
+        </h2>
       );
       break;
     case 'h3':
       headingToRender = (
-        <h3 className={`header__heading ${headingClassName}`}>{heading}</h3>
+        <h3 className={`header__heading ${headingClassNamesToAdd}`}>
+          {heading}
+        </h3>
       );
       break;
     case 'h4':
       headingToRender = (
-        <h4 className={`header__heading ${headingClassName}`}>{heading}</h4>
+        <h4 className={`header__heading ${headingClassNamesToAdd}`}>
+          {heading}
+        </h4>
       );
       break;
     case 'h5':
       headingToRender = (
-        <h5 className={`header__heading ${headingClassName}`}>{heading}</h5>
+        <h5 className={`header__heading ${headingClassNamesToAdd}`}>
+          {heading}
+        </h5>
       );
       break;
     case 'h6':
       headingToRender = (
-        <h6 className={`header__heading ${headingClassName}`}>{heading}</h6>
+        <h6 className={`header__heading ${headingClassNamesToAdd}`}>
+          {heading}
+        </h6>
       );
       break;
     default:
   }
 
   return (
-    <header className={`header ${className}`}>
+    <header className={classNamesToAdd}>
       {headingToRender}
       {children}
     </header>
