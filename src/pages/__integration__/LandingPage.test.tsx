6b5b65189a64ec2e4a781a8417f2ef873e269e21
= <7rubengarcia7@gmail.com>
2019-12-20 12:18:16 -0800

    [Testing](LandingPage): check that modal, with register form, is rendered when user clicks on register button

---
+++ src/pages/__integration__/LandingPage.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  render,
+  cleanup,
+  fireEvent,
+  RenderResult,
+  waitForElement,
+} from '@testing-library/react';
+
+import { LandingPage } from '..';
+
+describe('LandingPage Integration', () => {
+  it('should render', () => {
+    const { container }: RenderResult = render(<LandingPage />);
+
+    expect(container).toBeInTheDocument();
+    cleanup();
+  });
+
+  describe('User interactions', () => {
+    let container: RenderResult;
+
+    beforeEach(() => {
+      container = render(<LandingPage />);
+    });
+
+    it('should open a modal with RegisterPage when user clicks on Register button', async () => {
+      const registerButton = container.getByText('Register');
+
+      fireEvent.click(registerButton);
+
+      const registerModal = await waitForElement(() =>
+        container.getByText('Register | Create An Account')
+      );
+
+      expect(registerModal).toBeDefined();
+    });
+  });
+});
