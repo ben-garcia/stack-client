89467293efb5d0553adf3b86773f86e2aad6ce33
= <7rubengarcia7@gmail.com>
2019-12-25 15:56:30 -0800

    [Testing](RegisterPage): added integration tests

--- setupTests.ts
+++ setupTests.ts
@@ -1,5 +1,30 @@
+/* eslint-disable no-console */
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import '@testing-library/jest-dom/extend-expect';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// https://github.com/testing-library/react-testing-library/issues/281#issuecomment-480349256
+// this is just a little hack to silence a warning that we'll get until react
+// fixes this: https://github.com/facebook/react/pull/14853
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args: string[]) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return;
+    }
+    if (
+      /Warning: Can't perform a React state update on an unmounted component/.test(
+        args[0]
+      )
+    ) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
---
+++ src/pages/__integration__/RegisterPage.test.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  render,
+  cleanup,
+  fireEvent,
+  RenderResult,
+  waitForElement,
+} from '@testing-library/react';
+
+import { RegisterPage } from '..';
+
+describe('RegisterPage Integration', () => {
+  it('should render', () => {
+    const { container }: RenderResult = render(<RegisterPage />);
+
+    expect(container).toBeInTheDocument();
+    cleanup();
+  });
+
+  describe('user interactions', () => {
+    let result: RenderResult;
+
+    beforeEach(() => {
+      result = render(<RegisterPage />);
+    });
+    afterEach(cleanup);
+
+    it('should change value of email when user types', () => {
+      const emailInput = result.getByLabelText(/email/, { selector: 'input' });
+      expect(emailInput.getAttribute('value')).toBe('');
+      fireEvent.change(emailInput, {
+        target: { value: 'testing@testing.com' },
+      });
+      expect(emailInput.getAttribute('value')).toBe('testing@testing.com');
+    });
+
+    it('should change value of username when user types', () => {
+      const usernameInput = result.getByLabelText(/username/, {
+        selector: 'input',
+      });
+      expect(usernameInput.getAttribute('value')).toBe('');
+      fireEvent.change(usernameInput, {
+        target: { value: 'testing' },
+      });
+      expect(usernameInput.getAttribute('value')).toBe('testing');
+    });
+
+    it('should change value of username when user types', () => {
+      const passwordInput = result.getByLabelText(/password/, {
+        selector: 'input',
+      });
+      expect(passwordInput.getAttribute('value')).toBe('');
+      fireEvent.change(passwordInput, {
+        target: { value: 'password' },
+      });
+      expect(passwordInput.getAttribute('value')).toBe('password');
+    });
+
+    it('should enable submit button when input fields have been validated', async () => {
+      const submitButton = result.getByText('Register');
+      const emailInput = result.getByLabelText(/email/, {
+        selector: 'input',
+      });
+      const usernameInput = result.getByLabelText(/username/, {
+        selector: 'input',
+      });
+      const passwordInput = result.getByLabelText(/password/, {
+        selector: 'input',
+      });
+
+      expect(submitButton.getAttribute('disabled')).toBeDefined();
+
+      fireEvent.change(emailInput, { target: { value: 'testing@test.com' } });
+      fireEvent.change(usernameInput, { target: { value: 'username' } });
+      fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+      await waitForElement(() => submitButton);
+
+      expect(submitButton.getAttribute('disabled')).toBeFalsy();
+    });
+  });
+});
