341c0adf24b677c30209b475bb4e3caebeea2869
= <7rubengarcia7@gmail.com>
2019-12-25 09:27:50 -0800

    [Update](RegisterPage/utils): decreased min by 1 so that the UI isn't delayed

--- src/pages/RegisterPage/utils/index.ts
+++ src/pages/RegisterPage/utils/index.ts
@@ -2,20 +2,19 @@ import * as yup from 'yup';
 
 export const emailSchema = yup
   .string()
-  .trim()
   .email()
   .required();
 
 export const usernameSchema = yup
   .string()
   .trim()
-  .min(3)
+  .min(2, 'field must contain a minumun length of 3')
   .required();
 
 export const passwordSchema = yup
   .string()
   .trim()
-  .min(6)
+  .min(5, 'field must contain a minumum length of 6')
   .required();
 
 export const newUserSchema = yup.object().shape({
