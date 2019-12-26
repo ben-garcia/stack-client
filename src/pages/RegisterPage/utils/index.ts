1f964cde55ad84a8d7944d50a940cbdfd3a72925
= <7rubengarcia7@gmail.com>
2019-12-26 10:49:18 -0800

    [Update](RegisterPage/utils): added types to schemas

--- src/pages/RegisterPage/utils/index.ts
+++ src/pages/RegisterPage/utils/index.ts
@@ -1,23 +1,25 @@
 import * as yup from 'yup';
 
-export const emailSchema = yup
+import { User } from '../types';
+
+export const emailSchema: yup.StringSchema<string> = yup
   .string()
   .email()
   .required();
 
-export const usernameSchema = yup
+export const usernameSchema: yup.StringSchema<string> = yup
   .string()
   .trim()
   .min(2, 'minumun length of 3')
   .required();
 
-export const passwordSchema = yup
+export const passwordSchema: yup.StringSchema<string> = yup
   .string()
   .trim()
   .min(5, 'minumum length of 6')
   .required();
 
-export const newUserSchema = yup.object().shape({
+export const newUserSchema: yup.ObjectSchema<User> = yup.object().shape({
   email: emailSchema,
   username: usernameSchema,
   password: passwordSchema,
