b980ee200200e32a2fc15ff17ee7b6e2217b8536
= <7rubengarcia7@gmail.com>
2019-12-30 12:43:53 -0800

    {Update](Modal): added story for LoginPage

--- src/components/Modal/stories.tsx
+++ src/components/Modal/stories.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Modal from '.';
-import RegisterPage from '../../pages/RegisterPage';
+import { RegisterPage, LoginPage } from '../../pages';
 
 storiesOf('Modal', module)
   .add('default', () => <Modal header="Modal Header">Modal Example</Modal>)
   .add('with RegisterPage', () => (
     <Modal header="Register">
       <RegisterPage />
+    </Modal>
+  ))
+  .add('with LoginPage', () => (
+    <Modal header="Login">
+      <LoginPage />
     </Modal>
   ));
