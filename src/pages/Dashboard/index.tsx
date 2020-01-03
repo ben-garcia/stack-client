b267aac4583d5faf58146924704d15e79f96276f
= <7rubengarcia7@gmail.com>
2020-01-03 08:00:18 -0800

    [Update](Dashboard): added html structure and styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-import { DashboardProps } from './types';
 import './styles.scss';
 
-const Dashboard: React.FC<DashboardProps> = () => {
-  return <div className="dashboard">Dashboard</div>;
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard">
+      <section className="dashboard__top-nav">Top Nav</section>
+      <aside className="dashboard__sidebar">
+        <section className="sidebar-workspaces">Workspaces</section>
+        <section className="sidebar-channels">Channel Nav</section>
+      </aside>
+      <main className="dashboard__main">Main</main>
+    </div>
+  );
 };
 
 export default Dashboard;
