2f21b83084c0d7198385e6314420dc39fdbf903a
= <7rubengarcia7@gmail.com>
2020-01-10 15:49:15 -0800

    [Testing](WorkspaceInfo): added user prop to tests

--- src/components/WorkspaceInfo/test.tsx
+++ src/components/WorkspaceInfo/test.tsx
@@ -3,9 +3,18 @@ import { shallow, ShallowWrapper } from 'enzyme';
 
 import WorkspaceInfo from '.';
 
+const user = {
+  isLoggedIn: true,
+  id: 1,
+  username: 'test',
+  email: 'test@test.com',
+  createdAt: 'testing',
+  updatedAt: 'testingtseting',
+};
+
 describe('<WorkspaceInfo />', () => {
   it('should render', () => {
-    const wrapper: ShallowWrapper = shallow(<WorkspaceInfo />);
+    const wrapper: ShallowWrapper = shallow(<WorkspaceInfo user={user} />);
 
     expect(wrapper.hasClass('workspace-info')).toBe(true);
   });
