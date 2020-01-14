02a0defbd9b742554503d7fa06670efe7ae1c5b5
= <7rubengarcia7@gmail.com>
2020-01-14 08:57:32 -0800

    [update](WorkspaceInfo): removed user prop in test

--- src/components/WorkspaceInfo/test.tsx
+++ src/components/WorkspaceInfo/test.tsx
@@ -3,18 +3,9 @@ import { shallow, ShallowWrapper } from 'enzyme';
 
 import WorkspaceInfo from '.';
 
-const user = {
-  isLoggedIn: true,
-  id: 1,
-  username: 'test',
-  email: 'test@test.com',
-  createdAt: 'testing',
-  updatedAt: 'testingtseting',
-};
-
 describe('<WorkspaceInfo />', () => {
   it('should render', () => {
-    const wrapper: ShallowWrapper = shallow(<WorkspaceInfo user={user} />);
+    const wrapper: ShallowWrapper = shallow(<WorkspaceInfo />);
 
     expect(wrapper.hasClass('workspace-info')).toBe(true);
   });
