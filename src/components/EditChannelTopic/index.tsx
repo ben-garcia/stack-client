ee1d6c9b1f66591b08db8713014e95ec7b38b4e5
= <7rubengarcia7@gmail.com>
2020-02-14 14:49:04 -0800

    [Refactor](EditChannelTopic): removed connect HOC with 'useDispatch' and 'useSelector'

--- src/components/EditChannelTopic/index.tsx
+++ src/components/EditChannelTopic/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
-import { connect } from 'react-redux';
 
 import { Button, Form } from 'components';
 import sendRequest from 'api';
@@ -10,11 +10,13 @@ import { EditChannelTopicProps } from './types';
 import './styles.scss';
 
 const EditChannelTopic: React.FC<EditChannelTopicProps> = ({
-  currentChannel,
   setOpenEditModal,
-  updateChannelTopicAction,
   value,
 }) => {
+  const dispatch: Dispatch = useDispatch();
+  const { currentChannel } = useSelector((state: AppState) => ({
+    currentChannel: state.currentChannel,
+  }));
   const [topic, setTopic] = useState<string>(value || '');
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -29,7 +31,7 @@ const EditChannelTopic: React.FC<EditChannelTopicProps> = ({
     // save the channel topic to local storage
     localStorage.setItem('currentChannelTopic', topic);
     // dispatch action to update the current channel topic
-    updateChannelTopicAction(topic);
+    dispatch(updateChannelTopic(topic));
     // close the modal
     setOpenEditModal(false);
   };
@@ -49,15 +51,4 @@ const EditChannelTopic: React.FC<EditChannelTopicProps> = ({
   );
 };
 
-const mapStateToProps = (
-  state: AppState
-): Pick<AppState, 'currentChannel'> => ({
-  currentChannel: state.currentChannel,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  updateChannelTopicAction: (topic: string) =>
-    dispatch(updateChannelTopic(topic)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(EditChannelTopic);
+export default EditChannelTopic;
--- src/components/EditChannelTopic/types.ts
+++ src/components/EditChannelTopic/types.ts
@@ -1,9 +1,4 @@
-import { updateChannelTopic } from 'store/channel/actions';
-import { ChannelState } from 'store/channel/types';
-
 export interface EditChannelTopicProps {
-  currentChannel: ChannelState;
   setOpenEditModal: (state: boolean) => void;
-  updateChannelTopicAction: typeof updateChannelTopic;
   value?: string;
 }
