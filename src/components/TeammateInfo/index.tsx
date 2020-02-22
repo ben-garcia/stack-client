c38a2cee8b66ee7783d87506652f4ebdb2c29c9f
= <7rubengarcia7@gmail.com>
2020-02-22 15:21:58 -0800

    [Update](TeammateInfo): added static 'active' for now

--- src/components/TeammateInfo/index.tsx
+++ src/components/TeammateInfo/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { Icon, Text } from 'components';
+import { AppState } from 'store';
 import { TeammateInfoProps } from './types';
 import './styles.scss';
 
@@ -8,6 +10,9 @@ const TeammateInfo: React.FC<TeammateInfoProps> = ({
   teammate,
   className = '',
 }) => {
+  const { user } = useSelector((state: AppState) => ({
+    user: state.user,
+  }));
   let classesToAdd: string = 'teammate';
 
   if (className?.trim() !== '') {
@@ -16,15 +21,25 @@ const TeammateInfo: React.FC<TeammateInfoProps> = ({
 
   return (
     <section className={classesToAdd}>
-      <Text tag="div" size="md" className="teammate__name">
+      <Text tag="div" className="teammate__name">
         {teammate?.username}
+        {user.username === teammate?.username ? (
+          <Text className="teammate__optional-info" tag="span">
+            (you)
+          </Text>
+        ) : null}
       </Text>
-      <Icon
-        type="circle"
-        color="green"
-        size="xm"
-        className="teammate__circle-icon"
-      />
+      <div>
+        <Icon
+          type="circle"
+          color="green"
+          size="xm"
+          className="teammate__circle-icon"
+        />
+        <Text className="teammate__status" tag="span" size="xm">
+          active
+        </Text>
+      </div>
     </section>
   );
 };
