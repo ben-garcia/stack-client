733b5099dbc052176889db340dee1eb197c20a5f
= <7rubengarcia7@gmail.com>
2020-03-01 09:13:57 -0800

    [Update](CreateMessage): sending request to crate new message and updating store with new message

--- src/components/CreateMessage/index.tsx
+++ src/components/CreateMessage/index.tsx
@@ -1,18 +1,51 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import sendRequest from 'api';
 import { Form } from 'components';
+import { AppState } from 'store';
+import { addMessage } from 'store/messages';
 import { CreateMessageProps } from './types';
 import './styles.scss';
 
 const CreateMessage: React.FC<CreateMessageProps> = () => {
+  const dispatch = useDispatch();
+  const { currentChannelId, userId } = useSelector((state: AppState) => ({
+    currentChannelId: state.currentChannel.id,
+    userId: state.user.id,
+  }));
   const [message, setMessage] = useState<string>('');
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
   };
-  const handleKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  const handleKeyUp = async (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.keyCode === 13) {
-      // eslint-disable-next-line
-      console.log('submit');
+      try {
+        // data to send to the server in the request object
+        const data: any = {
+          message: {
+            channel: currentChannelId,
+            content: message,
+            user: userId,
+          },
+        };
+
+        const {
+          data: { message: newMessage },
+        } = await sendRequest({
+          method: 'POST',
+          url: '/messages',
+          data,
+        });
+
+        // clear the text from textarea
+        setMessage('');
+        // dispatch action to add newly created message to the store
+        dispatch(addMessage(newMessage));
+      } catch (err) {
+        // eslint-disable-next-line
+        console.log(err);
+      }
     }
   };
 
