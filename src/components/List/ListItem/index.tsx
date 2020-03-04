81d805e416ab5d0c96e448ecf747626d52e72b03
= <7rubengarcia7@gmail.com>
2020-03-04 07:31:13 -0800

    [Update](List/ListItem): added prop to determine hoverability

--- src/components/List/ListItem/index.tsx
+++ src/components/List/ListItem/index.tsx
@@ -7,8 +7,13 @@ const ListItem: React.FC<ListItemProps> = ({
   active = false,
   children,
   className = '',
+  hover = true,
 }) => {
   let classesToAdd: string = 'list__item';
+
+  if (hover) {
+    classesToAdd += ' list__item--hoverable';
+  }
 
   if (className.trim() !== '') {
     classesToAdd += ` ${className}`;
--- src/components/List/ListItem/types.ts
+++ src/components/List/ListItem/types.ts
@@ -2,4 +2,5 @@ export interface ListItemProps {
   active?: boolean;
   children: React.ReactNode;
   className?: string;
+  hover?: boolean; // don't include hover state
 }
