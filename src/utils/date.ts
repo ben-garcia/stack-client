85351ec76312660ddf7e3d26e9ffc6648cb91488
= <7rubengarcia7@gmail.com>
2020-04-02 14:42:07 -0700

    [Update](utils/date): seperated the logic to chck if the message was created 1 day prior to a new function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,21 +1,21 @@
 // helper functions to format ISO string to local time and day
 
 const getDay = (day: number): string => {
-  switch (day) {
+  switch (day + 1) {
     case 1:
-      return 'Monday';
-    case 2:
-      return 'Tuesday';
-    case 3:
-      return 'Wednesday';
-    case 4:
-      return 'Thursday';
-    case 5:
-      return 'Friday';
-    case 6:
-      return 'Saturday';
-    default:
       return 'Sunday';
+    case 2:
+      return 'Monday';
+    case 3:
+      return 'Tuesday';
+    case 4:
+      return 'Wednesday';
+    case 5:
+      return 'Thursday';
+    case 6:
+      return 'Friday';
+    default:
+      return 'Saturday';
   }
 };
 
@@ -33,7 +33,7 @@ const getDate = (date: number): string => {
 };
 
 const getMonth = (month: number): string => {
-  switch (month) {
+  switch (month + 1) {
     case 1:
       return 'January';
     case 2:
@@ -61,6 +61,60 @@ const getMonth = (month: number): string => {
   }
 };
 
+const setupMap = (date: string): Map<number, number> => {
+  const monthMaxDays = new Map<number, number>();
+  monthMaxDays.set(1, 31);
+  // check for leap year
+  if (Number(date.split('/')[2]) / 4 === 0) {
+    monthMaxDays.set(2, 29);
+  } else {
+    monthMaxDays.set(2, 28);
+  }
+  monthMaxDays.set(3, 31);
+  monthMaxDays.set(4, 30);
+  monthMaxDays.set(5, 31);
+  monthMaxDays.set(6, 30);
+  monthMaxDays.set(7, 31);
+  monthMaxDays.set(8, 31);
+  monthMaxDays.set(9, 30);
+  monthMaxDays.set(10, 31);
+  monthMaxDays.set(11, 30);
+  monthMaxDays.set(12, 31);
+
+  return monthMaxDays;
+};
+
+const isMessageFromYesterday = (
+  today: string,
+  dateInQuestion: string
+): boolean => {
+  const monthMaxDays = setupMap(today);
+  const month = Number(dateInQuestion.split('/')[0]);
+  const date = Number(dateInQuestion.split('/')[1]);
+  const year = Number(dateInQuestion.split('/')[2]);
+  const todayMonth = Number(today.split('/')[0]);
+  const todayDate = Number(today.split('/')[1]);
+  const todayYear = Number(today.split('/')[2]);
+
+  // check that it isn't the 1st of the month
+  if (date === 1) {
+    const todayToCompare = `${todayMonth}/${todayDate - 1}/${todayYear}`;
+    if (todayToCompare === dateInQuestion) {
+      return true;
+    }
+    // check that month isn't January and date is 1
+  } else if (date === 1 && month !== 1) {
+    const newDate = monthMaxDays.get(month - 1);
+    const dateToCompare = `${month - 1}/${newDate}/${year}`;
+    const todayToCompare = `${todayMonth}/${todayDate - 1}/${todayYear}`;
+    if (dateToCompare === todayToCompare) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 const getCurrentYear = (): number => new Date().getFullYear();
 
 const printFormattedDate = (ISOString: string): string => {
@@ -68,22 +122,18 @@ const printFormattedDate = (ISOString: string): string => {
   // if the current year is equal to the ISOString year
   // then format string like so: month date, year
   if (getCurrentYear() === dateToFormat.getFullYear()) {
-    const dateInMilliseconds = Date.parse(ISOString);
-    const currentDateInMilliseconds = Date.parse(new Date().toString());
-    const dayInMilliseconds = 1000 * 60 * 60 * 24;
-    // for those messages that were created in the current date day month and year
-    if (dayInMilliseconds > currentDateInMilliseconds - dateInMilliseconds) {
+    const todaysDate = new Date().toLocaleDateString();
+    const dateInQuestion = dateToFormat.toLocaleDateString();
+    // for those messages that were created in the current day month and year
+    if (todaysDate === dateInQuestion) {
       return 'Today';
     }
-    // messages created 1 day before
-    if (
-      dayInMilliseconds < currentDateInMilliseconds - dateInMilliseconds &&
-      dayInMilliseconds * 2 > currentDateInMilliseconds - dateInMilliseconds
-    ) {
+    if (isMessageFromYesterday(todaysDate, dateInQuestion)) {
+      // messages created 1 day before
       return 'Yesterday';
     }
     // otherwise: day, month date
-    return `${getDay(dateToFormat.getDate())}, ${getMonth(
+    return `${getDay(dateToFormat.getDay())}, ${getMonth(
       dateToFormat.getMonth()
     )} ${getDate(dateToFormat.getDate())}`;
   }
