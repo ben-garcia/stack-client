64f6437f15c6b7f7172ded7adae0593273f0d902
= <7rubengarcia7@gmail.com>
2020-04-16 17:23:52 -0700

    [Update](ChannelDetails): rendering when user clicks the details button in ChannelView with styles

--- src/components/ChannelDetails/index.tsx
+++ src/components/ChannelDetails/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Button, Icon, Text } from 'components';
+import { Button, Icon, List, Text } from 'components';
 import { AppState } from 'store';
 import { closeChannelDetails } from 'store/channelDetails';
 import { openInvitePeopleModal } from 'store/invitePeopleModal';
+import { Member } from 'store/members';
+import { printFormattedDate } from 'utils';
 import './styles.scss';
 
 const ChannelDetails = () => {
   const dispatch = useDispatch();
-  const { currentChannel, members } = useSelector((state: AppState) => ({
+  const { currentChannel, members, user } = useSelector((state: AppState) => ({
     currentChannel: state.currentChannel,
     members: state.members.list,
+    user: state.user,
   }));
+  // keep track of when the user clicks the about dropdown
+  const [aboutDropdownIsOpen, setAboutDropdownIsOpen] = useState<boolean>(
+    false
+  );
+  // keep track of when the user clicks the members dropdown
+  const [membersDropdownIsOpen, setMembersDropdownIsOpen] = useState<boolean>(
+    false
+  );
 
   return (
     <section className="channel-details">
@@ -31,46 +42,170 @@ const ChannelDetails = () => {
           onClick={() => dispatch(closeChannelDetails())}
           type="button"
         >
-          <Icon type="times" size="sm" />
-        </Button>
-      </div>
-      <div className="channel-details__inner-two">
-        <Button
-          className="channel-details__add-people-button"
-          color="transparent"
-          onClick={() => dispatch(openInvitePeopleModal())}
-          type="button"
-          title={`Add people to #${currentChannel.name}`}
-        >
-          <Icon className="channel-details__user-icon" size="sm" type="user" />
-          <Text size="xm" tag="span">
-            Add
-          </Text>
-        </Button>
-      </div>
-      <div className="channel-details__about">
-        <Text size="sm" tag="span">
-          About
-        </Text>
-        <Icon
-          className="channel-details__icon-chevron-right"
-          size="xm"
-          type="chevron-right"
-        />
-      </div>
-      <div className="channel-details__members">
-        <Text size="sm" tag="span">
-          Members
-        </Text>
-        <div className="channel-details__inner-three">
-          <Text className="channel-details__member-count" size="xm" tag="span">
-            {members.length}
-          </Text>
           <Icon
-            className="channel-details__icon-chevron-right"
-            size="xm"
-            type="chevron-right"
+            className="channel-details__close-icon"
+            type="times"
+            size="sm"
           />
+        </Button>
+      </div>
+      <div className="channel-details__container">
+        <div className="channel-details__inner-two">
+          <Button
+            className="channel-details__add-people-button"
+            color="transparent"
+            onClick={() => dispatch(openInvitePeopleModal())}
+            type="button"
+            title={`Add people to #${currentChannel.name}`}
+          >
+            <Icon
+              className="channel-details__user-icon"
+              size="sm"
+              type="user"
+            />
+            <Text size="xm" tag="span">
+              Add
+            </Text>
+          </Button>
+        </div>
+        <div className="channel-details__about">
+          <Button
+            className="channel-details__dropdown-button"
+            color="transparent"
+            onClick={() => {
+              if (membersDropdownIsOpen) {
+                setMembersDropdownIsOpen(false);
+              }
+              setAboutDropdownIsOpen(!aboutDropdownIsOpen);
+            }}
+            type="button"
+          >
+            <Text size="sm" tag="span">
+              About
+            </Text>
+            <Icon
+              className={
+                aboutDropdownIsOpen
+                  ? 'channel-details__icon-chevron-right channel-details__icon-chevron-right--translate-rotate '
+                  : 'channel-details__icon-chevron-right'
+              }
+              size="xm"
+              type="chevron-right"
+            />
+          </Button>
+          <div
+            className="channel-details__about-info"
+            style={
+              aboutDropdownIsOpen ? { display: 'block' } : { display: 'none' }
+            }
+          >
+            <div className="channel-details__topic">
+              <Text
+                className="channel-details__subtext channel-details__subtext--dark"
+                tag="div"
+                size="xm"
+              >
+                Topic
+              </Text>
+              {currentChannel.topic ? (
+                <Text className="channel-details__subtext" tag="div">
+                  {currentChannel.topic}
+                </Text>
+              ) : (
+                <Text className="channel-details__subtext" tag="div" size="xm">
+                  Whats up for discussion?
+                </Text>
+              )}
+              <Button
+                className="channel-details__edit-button"
+                color="transparent"
+                type="button"
+              >
+                <Text size="xm">edit</Text>
+              </Button>
+            </div>
+            <div className="channel-details__description">
+              <Text
+                className="channel-details__subtext channel-details__subtext--dark"
+                tag="div"
+                size="xm"
+              >
+                Description
+              </Text>
+              {currentChannel.description ? (
+                <Text className="channel-details__subtext" tag="div" size="xm">
+                  {currentChannel.description}
+                </Text>
+              ) : (
+                <Text className="channel-details__subtext" tag="div" size="xm">
+                  Whats up for discussion?
+                </Text>
+              )}
+              <Button
+                className="channel-details__edit-button"
+                color="transparent"
+                type="button"
+              >
+                <Text size="xm">edit</Text>
+              </Button>
+            </div>
+            <Text className="channel-details__created" size="xm" tag="div">
+              {`Created ${printFormattedDate(
+                currentChannel.createdAt as string
+              )}`}
+            </Text>
+          </div>
+        </div>
+        <div className="channel-details__members">
+          <Button
+            className="channel-details__dropdown-button"
+            color="transparent"
+            onClick={() => {
+              if (aboutDropdownIsOpen) {
+                setAboutDropdownIsOpen(false);
+              }
+              setMembersDropdownIsOpen(!membersDropdownIsOpen);
+            }}
+            type="button"
+          >
+            <Text size="sm" tag="span">
+              Members
+            </Text>
+            <div className="channel-details__inner-three">
+              <Text
+                className="channel-details__member-count"
+                size="xm"
+                tag="span"
+              >
+                {members.length}
+              </Text>
+              <Icon
+                className={
+                  membersDropdownIsOpen
+                    ? 'channel-details__icon-chevron-right channel-details__icon-chevron-right--rotate'
+                    : 'channel-details__icon-chevron-right'
+                }
+                size="xm"
+                type="chevron-right"
+              />
+            </div>
+          </Button>
+          <div
+            style={
+              membersDropdownIsOpen ? { display: 'block' } : { display: 'none' }
+            }
+          >
+            <List className="channel-details__members-info members-list">
+              {members.map((m: Member) => (
+                <List.Item className="members-list__item" hover={false}>
+                  <Icon className="members-list__icon" size="sm" type="user" />
+                  {user.username === m.username
+                    ? `${m.username}(you)`
+                    : m.username}
+                </List.Item>
+              ))}
+            </List>
+          </div>
         </div>
       </div>
     </section>
