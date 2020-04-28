ce6d9831e22f35c004c2ceeceef9759ff6ffc755
= <7rubengarcia7@gmail.com>
2020-04-28 15:42:51 -0700

    [Update]: removed 'channelName' when emitting messages(that is handle on the server) && when emitting an event the server sends it all clients(including sender), so dispatching action to change store after the client has received it

--- src/components/CreateMessage/index.tsx
+++ src/components/CreateMessage/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector, useStore } from 'react-redux';
 import io from 'socket.io-client';
 
-import sendRequest from 'api';
+// import sendRequest from 'api';
 import { Form } from 'components';
 import { AppState } from 'store';
 import { addUserDirectMessage } from 'store/directMessages';
@@ -37,12 +37,12 @@ const CreateMessage: React.FC<CreateMessageProps> = () => {
 
       try {
         let data: any;
-        let url: string = '';
+        // let url: string = '';
         // since the redux store expects messages to have an id(key prop)
         // use a random number until page refresh
         const randomNumber = Math.random();
         if (currentChannel.id && !currentTeammate.id && currentWorkspace.id) {
-          url = '/messages';
+          // url = '/messages';
           // data to send to the server in the request object
           data = {
             message: {
@@ -51,21 +51,12 @@ const CreateMessage: React.FC<CreateMessageProps> = () => {
               user: user.id,
             },
           };
-          // dispatch action to add newly created message to the store
-          dispatch(
-            addMessage({
-              ...data.message,
-              id: randomNumber,
-              createdAt: new Date().toISOString(),
-              user: { username: user.username },
-            })
-          );
         } else if (
           currentTeammate.id &&
           !currentChannel.id &&
           currentWorkspace.id
         ) {
-          url = '/direct-messages';
+          // url = '/direct-messages';
           data = {
             message: {
               content: message,
@@ -73,15 +64,6 @@ const CreateMessage: React.FC<CreateMessageProps> = () => {
               workspaceId: currentWorkspace.id,
             },
           };
-          // dispatch action to add new created direct message
-          dispatch(
-            addUserDirectMessage({
-              ...data.message,
-              id: randomNumber,
-              createdAt: new Date().toISOString(),
-              user: { username: user.username },
-            })
-          );
         }
 
         // make sure their is an active socket open
@@ -95,20 +77,12 @@ const CreateMessage: React.FC<CreateMessageProps> = () => {
                 createdAt: new Date().toISOString(),
                 updatedAt: new Date().toISOString(),
                 user: { username: user.username },
-                channelName: `${currentWorkspace.id}:${currentWorkspace.name}-${currentChannel.id}-${currentChannel.name}`,
               })
             );
           }
           if (currentTeammate.id && !currentChannel.id) {
             // don't emit a message to yourself
             if (currentTeammate.id !== user.id) {
-              // set up array with the ids of both users
-              // then sort the array to get the unique(ids are unique per user) channel name
-              const ids = [
-                { id: user.id!, username: user.username },
-                { id: currentTeammate.id!, username: currentTeammate.username },
-              ];
-              const channelName = ids.sort((a, b) => a.id - b.id);
               socket.emit(
                 'direct-message',
                 JSON.stringify({
@@ -118,26 +92,28 @@ const CreateMessage: React.FC<CreateMessageProps> = () => {
                   updatedAt: new Date().toISOString(),
                   user: { username: user.username },
                   workspaceId: currentWorkspace.id,
-                  channelName: `${channelName[0].id}:${channelName[0].username}-${channelName[1].id}:${channelName[1].username}`,
                 })
               );
             }
           }
         }
 
-        await sendRequest({
-          method: 'POST',
-          url,
-          data,
-        });
+        // await sendRequest({
+        //   method: 'POST',
+        //   url,
+        //   data,
+        // });
       } catch (err) {
         // eslint-disable-next-line
-        console.log(err);
+        console.log('error in CreateMessage handleKeyUp: ', err);
       }
     }
   };
 
   useEffect(() => {
+    if (socket) {
+      socket.close();
+    }
     const mySocket = io.connect('http://localhost:8080');
     setSocket(mySocket);
     if (currentChannel.id && !currentTeammate.id) {
@@ -146,8 +122,7 @@ const CreateMessage: React.FC<CreateMessageProps> = () => {
         username: user.username,
         workspaceName: `${currentWorkspace.id}:${currentWorkspace.name}`,
       });
-    }
-    if (currentTeammate.id && !currentChannel.id) {
+    } else if (currentTeammate.id && !currentChannel.id) {
       // set up array with the ids of both users
       // then sort the array to get the unique(ids are unique per user) channel name
       const ids = [
@@ -182,16 +157,12 @@ const CreateMessage: React.FC<CreateMessageProps> = () => {
       dispatch(addMessage(JSON.parse(channelMessage)));
     });
     mySocket.on('direct-message', (channelMessage: any) => {
-      // eslint-disable-next-line
-      console.log('direct-message: ', JSON.parse(channelMessage));
       // update the store so that the new direct message will render
       dispatch(addUserDirectMessage(JSON.parse(channelMessage)));
     });
     mySocket.on('user-disconnected', (username: any) => {
       // dispatch action to set the username to away status
       dispatch(teammateDisconnected(username));
-      // eslint-disable-next-line
-      console.log('user-disconnected', username);
     });
     // run before component unmounts
     // clean disconnect the socket
