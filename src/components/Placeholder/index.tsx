e3243e1d8e18838add3be881050489651dc19fde
= <7rubengarcia7@gmail.com>
2020-05-05 15:37:16 -0700

    [Update]([0]ChannelView, [1]Placeholder): [0] renderng Placeholder when the network request is pending, [1] added 'numberOfTags' prop to know the number of times to render

--- src/components/Placeholder/index.tsx
+++ src/components/Placeholder/index.tsx
@@ -6,6 +6,7 @@ import './styles.scss';
 const Placeholder: React.FC<PlaceholderProps> = ({
   className = '',
   height = '',
+  numberOfTags = 1,
   type,
   width = '',
 }) => {
@@ -22,8 +23,14 @@ const Placeholder: React.FC<PlaceholderProps> = ({
     styles.width = width;
   }
 
+  // used to get the number of tags to render
+  const arr: number[] = [];
+  for (let i = 0; i < numberOfTags; i += 1) {
+    arr.push(i);
+  }
+
   if (type === 'message') {
-    jsxToRender = (
+    jsxToRender = arr.map(() => (
       <div className="placeholder__message p-message">
         <div className="p-message__date" />
         <div className="p-message__inner">
@@ -36,9 +43,9 @@ const Placeholder: React.FC<PlaceholderProps> = ({
           </div>
         </div>
       </div>
-    );
+    ));
   } else if (type === 'list') {
-    jsxToRender = (
+    jsxToRender = arr.map(() => (
       <div className="placeholder__list">
         <div className="p-wrapper">
           <div className="horizontal-bar" />
@@ -49,14 +56,14 @@ const Placeholder: React.FC<PlaceholderProps> = ({
           <div className="horizontal-bar horizontal-bar--indented" />
         </div>
       </div>
-    );
+    ));
   } else {
-    jsxToRender = (
+    jsxToRender = arr.map(() => (
       <div className="placeholder__info p-info">
         <div className="horizontal-bar horizontal-bar--extended" />
         <div className="horizontal-bar horizontal-bar--extended" />
       </div>
-    );
+    ));
   }
 
   return (
--- src/components/Placeholder/types.ts
+++ src/components/Placeholder/types.ts
@@ -1,6 +1,7 @@
 export interface PlaceholderProps {
   className?: string;
   height?: string;
+  numberOfTags?: number;
   type: 'info' | 'list' | 'message';
   width?: string;
 }
