c7918578190546f4ca2da699cc7fc95803b00d26
= <7rubengarcia7@gmail.com>
2020-05-06 12:12:13 -0700

    [Update]([0]ChannelView, [1]WorkspaceSidebar): [0] removed Placeholder component, [1] added Placeholder

--- src/components/ChannelView/index.tsx
+++ src/components/ChannelView/index.tsx
@@ -11,7 +11,6 @@ import {
   Icon,
   MessageList,
   Modal,
-  Placeholder,
   Scrollbar,
   Text,
   WorkspaceInfo,
@@ -34,7 +33,6 @@ const ChannelView: React.FC<ChannelViewProps> = ({ className = '' }) => {
     currentChannel,
     currentTeammate,
     editChannelDescriptionModalIsOpen,
-    messages,
     user,
   } = useSelector((state: AppState) => ({
     addPeopleModalIsOpen: state.addPeopleModalIsOpen,
@@ -42,7 +40,6 @@ const ChannelView: React.FC<ChannelViewProps> = ({ className = '' }) => {
     currentChannel: state.currentChannel,
     currentTeammate: state.currentTeammate,
     editChannelDescriptionModalIsOpen: state.editChannelDescriptionModalIsOpen,
-    messages: state.messages,
     user: state.user,
   }));
   let classesToAdd: string = 'main-container';
@@ -60,161 +57,159 @@ const ChannelView: React.FC<ChannelViewProps> = ({ className = '' }) => {
           containerHeight="84vh"
           scrollbarPositionStartAtBottom
         >
-          {messages.list.length > 0 && !messages.isLoading ? (
-            <div>
-              {currentChannel.id !== 0 && !currentTeammate.id && (
-                <div>
-                  <h1 className="channel-view__inner">
-                    <Icon
-                      className="channel-view__hash-icon"
-                      type="hash"
-                      size="xm"
-                    />
-                    <Text className="channel-view__name" tag="span">
-                      {currentChannel.name}
-                    </Text>
-                  </h1>
-                  <div className="channel-view__inner">
-                    <Text tag="span" size="sm">
-                      You created this channel on
-                    </Text>
-                    <Text
-                      className="channel-view__created-at"
-                      tag="span"
-                      size="sm"
-                    >
-                      {printFormattedDate(currentChannel.createdAt!)}
-                    </Text>
-                    <Text tag="span" size="sm">
-                      This is the very beginning of the
-                    </Text>
-                    <strong className="channel-view__inner-two">
-                      <Icon type="hash" size="sm" />
-                      <Text tag="span">{currentChannel.name}</Text>
-                    </strong>
-                    {currentChannel.description && (
-                      <div className="channel-view__inner-three">
-                        <Text tag="span" size="sm">
-                          Description:
-                        </Text>
-                        <Text className="channel-view__description" tag="span">
-                          {currentChannel.description}
-                        </Text>
-                        <Text tag="span">
-                          (
-                          <Button
-                            className="channel-view__edit-button"
-                            type="button"
-                            color="transparent"
-                            title="Edit Channel Description"
-                            onClick={() =>
-                              dispatch(openEditChannelDescriptionModal())
-                            }
-                          >
-                            <Text tag="span" size="sm">
-                              edit
-                            </Text>
-                          </Button>
-                          )
-                        </Text>
-                        {editChannelDescriptionModalIsOpen && (
-                          <Modal
-                            header="Edit channel description"
-                            size="md"
-                            onClose={() =>
-                              dispatch(closeEditChannelDescriptionModal())
-                            }
-                          >
-                            <EditChannelDescription
-                              value={currentChannel?.description}
-                            />
-                          </Modal>
-                        )}
-                      </div>
-                    )}
-                    <div className="channel-view__inner-four">
-                      <Icon type="user" size="xm" />
-                      <Button
-                        className="channel-view__user-icon"
-                        type="button"
-                        color="transparent"
-                        onClick={() => dispatch(openAddPeopleModal())}
-                      >
-                        <Text tag="span" size="xm">
-                          Add people
-                        </Text>
-                      </Button>
-                      {addPeopleModalIsOpen && (
-                        <Modal
-                          header="Add People"
-                          size="md"
-                          onClose={() => dispatch(closeAddPeopleModal())}
+          <div>
+            {currentChannel.id !== 0 && !currentTeammate.id && (
+              <div>
+                <h1 className="channel-view__inner">
+                  <Icon
+                    className="channel-view__hash-icon"
+                    type="hash"
+                    size="xm"
+                  />
+                  <Text className="channel-view__name" tag="span">
+                    {currentChannel.name}
+                  </Text>
+                </h1>
+                <div className="channel-view__inner">
+                  <Text tag="span" size="sm">
+                    You created this channel on
+                  </Text>
+                  <Text
+                    className="channel-view__created-at"
+                    tag="span"
+                    size="sm"
+                  >
+                    {printFormattedDate(currentChannel.createdAt!)}
+                  </Text>
+                  <Text tag="span" size="sm">
+                    This is the very beginning of the
+                  </Text>
+                  <strong className="channel-view__inner-two">
+                    <Icon type="hash" size="sm" />
+                    <Text tag="span">{currentChannel.name}</Text>
+                  </strong>
+                  {currentChannel.description && (
+                    <div className="channel-view__inner-three">
+                      <Text tag="span" size="sm">
+                        Description:
+                      </Text>
+                      <Text className="channel-view__description" tag="span">
+                        {currentChannel.description}
+                      </Text>
+                      <Text tag="span">
+                        (
+                        <Button
+                          className="channel-view__edit-button"
+                          type="button"
+                          color="transparent"
+                          title="Edit Channel Description"
+                          onClick={() =>
+                            dispatch(openEditChannelDescriptionModal())
+                          }
                         >
-                          <AddPeople />
+                          <Text tag="span" size="sm">
+                            edit
+                          </Text>
+                        </Button>
+                        )
+                      </Text>
+                      {editChannelDescriptionModalIsOpen && (
+                        <Modal
+                          header="Edit channel description"
+                          size="md"
+                          onClose={() =>
+                            dispatch(closeEditChannelDescriptionModal())
+                          }
+                        >
+                          <EditChannelDescription
+                            value={currentChannel?.description}
+                          />
                         </Modal>
                       )}
                     </div>
+                  )}
+                  <div className="channel-view__inner-four">
+                    <Icon type="user" size="xm" />
+                    <Button
+                      className="channel-view__user-icon"
+                      type="button"
+                      color="transparent"
+                      onClick={() => dispatch(openAddPeopleModal())}
+                    >
+                      <Text tag="span" size="xm">
+                        Add people
+                      </Text>
+                    </Button>
+                    {addPeopleModalIsOpen && (
+                      <Modal
+                        header="Add People"
+                        size="md"
+                        onClose={() => dispatch(closeAddPeopleModal())}
+                      >
+                        <AddPeople />
+                      </Modal>
+                    )}
                   </div>
                 </div>
-              )}
-              {currentTeammate && currentChannel.id === 0 && (
-                <div className="channel-view__inner">
-                  {currentChannel.id || currentTeammate.id ? (
-                    <div className="c-teammate">
-                      <Icon
-                        className="c-teammate__user-icon"
-                        type="user"
-                        size="lg"
-                      />
-                      <Text className="c-teammate__username" tag="span">
+              </div>
+            )}
+            {currentTeammate && currentChannel.id === 0 && (
+              <div className="channel-view__inner">
+                {currentChannel.id || currentTeammate.id ? (
+                  <div className="c-teammate">
+                    <Icon
+                      className="c-teammate__user-icon"
+                      type="user"
+                      size="lg"
+                    />
+                    <Text className="c-teammate__username" tag="span">
+                      {currentTeammate.username}
+                    </Text>
+                  </div>
+                ) : null}
+                <div className="c-teammate__inner channel-view__inner">
+                  {user.id === currentTeammate.id && (
+                    <Text size="sm">
+                      <Text
+                        className="c-teammate__message"
+                        tag="span"
+                        size="sm"
+                      >
+                        This is your space.
+                      </Text>
+                      Draft messages, list your to-dos, or keep links and files
+                      handy. You can also talk to yourself here, but please bear
+                      in mind you’ll have to supply both sides of the
+                      conversation.
+                    </Text>
+                  )}
+                  {currentTeammate.id ? (
+                    <Text size="sm">
+                      This is the very beginning of your direct message history
+                      with
+                      <Text
+                        className="c-teammate__message c-teammate__message--margin-left"
+                        tag="span"
+                        size="sm"
+                      >
                         {currentTeammate.username}
                       </Text>
-                    </div>
+                    </Text>
                   ) : null}
-                  <div className="c-teammate__inner channel-view__inner">
-                    {user.id === currentTeammate.id && (
-                      <Text size="sm">
-                        <Text
-                          className="c-teammate__message"
-                          tag="span"
-                          size="sm"
-                        >
-                          This is your space.
-                        </Text>
-                        Draft messages, list your to-dos, or keep links and
-                        files handy. You can also talk to yourself here, but
-                        please bear in mind you’ll have to supply both sides of
-                        the conversation.
-                      </Text>
-                    )}
-                    {currentTeammate.id ? (
-                      <Text size="sm">
-                        This is the very beginning of your direct message
-                        history with
-                        <Text
-                          className="c-teammate__message c-teammate__message--margin-left"
-                          tag="span"
-                          size="sm"
-                        >
-                          {currentTeammate.username}
-                        </Text>
-                      </Text>
-                    ) : null}
-                  </div>
                 </div>
-              )}
-              <MessageList />
-            </div>
-          ) : (
-            <Placeholder numberOfTags={5} type="message" />
-          )}
-        </Scrollbar>
-        {(currentChannel.id && !currentTeammate.id) ||
-        (currentTeammate.id && !currentChannel.id) ? (
-          <div className="channel-view__message">
-            <CreateMessage />
+              </div>
+            )}
+            <MessageList />
           </div>
-        ) : null}
+        </Scrollbar>
+        <div>
+          {(currentChannel.id && !currentTeammate.id) ||
+          (currentTeammate.id && !currentChannel.id) ? (
+            <div className="channel-view__message">
+              <CreateMessage />
+            </div>
+          ) : null}
+        </div>
       </section>
       {channelDetails.isOpen && <ChannelDetails />}
     </main>
--- src/components/WorkspaceSidebar/index.tsx
+++ src/components/WorkspaceSidebar/index.tsx
@@ -1,12 +1,25 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { ChannelList, Scrollbar, TeammatesList, Workspace } from 'components';
+import {
+  ChannelList,
+  Placeholder,
+  Scrollbar,
+  TeammatesList,
+  Workspace,
+} from 'components';
+import { AppState } from 'store';
 import { WorkspaceSidebarProps } from './types';
 
 const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = ({
   className = '',
 }) => {
+  const { channels, teammates } = useSelector((state: AppState) => ({
+    channels: state.channels,
+    currentWorkspace: state.currentWorkspace,
+    teammates: state.teammates,
+  }));
   let classesToAdd: string = 'workspace-sidebar';
 
   if (className?.trim() !== '') {
@@ -15,11 +28,18 @@ const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = ({
 
   return (
     <div className={classesToAdd}>
-      <Workspace />
-      <Scrollbar color="light" containerHeight="92vh">
-        <ChannelList />
-        <TeammatesList />
-      </Scrollbar>
+      {(channels.list.length > 0 && !channels.isLoading) ||
+      (teammates.list.length > 0 && !teammates.isLoading) ? (
+        <>
+          <Workspace />
+          <Scrollbar color="light" containerHeight="92vh">
+            <ChannelList />
+            <TeammatesList />
+          </Scrollbar>
+        </>
+      ) : (
+        <Placeholder numberOfTags={2} type="list" />
+      )}
     </div>
   );
 };
