e1aaf12e9a3ad189a472bc5c3d99962371a9e176
= <7rubengarcia7@gmail.com>
2020-08-29 14:38:12 -0700

    [Add](changeChannelDescription): added e2e tests

---
+++ cypress/integration/changeChannelDescription.spec.ts
@@ -0,0 +1,50 @@
+describe('Change channel description', () => {
+  beforeEach(() => {
+    (cy as any).clearDB();
+    (cy as any).login();
+    (cy as any).addWorkspace({ name: 'CypressTest' });
+  });
+
+  it('should close the modal', () => {
+    (cy as any).addChannel({
+      name: 'channel 1',
+      description: 'channel description',
+      private: false,
+    });
+    cy.visit('/dashboard');
+    cy.get('button.channel-view__edit-button').click();
+    cy.contains('Edit channel description').should('be.visible');
+    cy.get('.modal').should('exist');
+    cy.get('.modal__button-close').click();
+    cy.get('.modal').should('not.exist');
+  });
+
+  it('should change channel description', () => {
+    (cy as any).addChannel({
+      name: 'channel 1',
+      description: 'old description',
+      private: false,
+    });
+    cy.visit('/dashboard');
+
+    const newDescription = 'new description';
+
+    cy.get('button.channel-view__edit-button').click();
+    cy.get('.form__textarea')
+      .as('textarea')
+      .should('have.text', 'old description');
+    cy.get('.form__textarea')
+      .clear()
+      .type(newDescription);
+    cy.get('@textarea').should('have.text', newDescription);
+    cy.get('button[type="submit"]')
+      .click()
+      .should(() => {
+        const currentChannel = JSON.parse(
+          localStorage.getItem('currentChannel') as string
+        );
+        expect(currentChannel.description).to.eq(newDescription);
+      });
+    cy.get('.channel-view__description').should('have.text', newDescription);
+  });
+});
