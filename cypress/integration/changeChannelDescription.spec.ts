e349ce3888306b865631ba2aa4ec886c59a6877a
= <7rubengarcia7@gmail.com>
2020-08-30 09:11:30 -0700

    [Update]([0]changeChannelDescription, [1]gitignore): [0] added cases for channel details pane, [1] included cypress videos and screenshots

--- cypress/integration/changeChannelDescription.spec.ts
+++ cypress/integration/changeChannelDescription.spec.ts
@@ -47,4 +47,39 @@ describe('Change channel description', () => {
       });
     cy.get('.channel-view__description').should('have.text', newDescription);
   });
+
+  it('should change channel description(via channel details)', () => {
+    (cy as any).addChannel({
+      name: 'channel 1',
+      description: 'old description',
+      private: false,
+    });
+    cy.visit('/dashboard');
+
+    const newDescription = 'new description';
+
+    cy.get('button.details-button').click();
+    cy.get('button.channel-details__dropdown-button')
+      .first()
+      .click();
+    cy.get('button.channel-details__edit-button')
+      .eq(1)
+      .click();
+    cy.get('.form__textarea')
+      .as('textarea')
+      .should('have.text', 'old description');
+    cy.get('.form__textarea')
+      .clear()
+      .type(newDescription);
+    cy.get('@textarea').should('have.text', newDescription);
+    cy.get('button[type="submit"]')
+      .click()
+      .should(() => {
+        const currentChannel = JSON.parse(
+          localStorage.getItem('currentChannel') as string
+        );
+        expect(currentChannel.description).to.eq(newDescription);
+      });
+    cy.get('.channel-view__description').should('have.text', newDescription);
+  });
 });
